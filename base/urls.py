01ea16194dc1db35da7aabbbc67d51fab5dc09af
RAihan Rabby <57155292+RAIHANRABBY@users.noreply.github.com>
2022-05-11 01:00:43 +0600

    login,logout

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
-from .views import homeView, roomView, createRoomView, updateRoomView
+from .views import  homeView, roomView, createRoomView, updateRoomView, deleteRoomView,loginpage,logoutpage
 
 urlpatterns = [
     path('home/', homeView, name='home'),
-    path('', homeView),
+    path('', homeView,name='home'),
     path('room/<str:pk>', roomView, name='room'),
     path('createroom/', createRoomView, name='create_room'),
     path('updateroom/<str:pk>', updateRoomView, name='update-room'),
+    path('deleteroom/<str:pk>', deleteRoomView, name='delete-room'),
+    path('login/', loginpage, name='login'),
+    path('logout/', logoutpage, name='logout'),
 
 ]
--- base/views.py
+++ base/views.py
@@ -1,25 +1,43 @@
 from django.shortcuts import render, redirect
-from .models import Messages, Room
+from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login,logout,authenticate
+from .models import Messages, Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 
+# this is the home page view
+
 
 def homeView(request):
-    rooms = Room.objects.all()
-    context = {"rooms": rooms}
+    # getting the data that is passed on the query from home page tamplate
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+# the ways the search option will work
+    rooms = Room.objects.filter(Q(topic__name__icontains=q) |
+                                Q(name__icontains=q) |
+                                Q(describtion__icontains=q)
+
+
+
+                                )
+
+    topic = Topic.objects.all()
+    room_count= rooms.count()
+    context = {"rooms": rooms, 'topic': topic,'room_count':room_count}
     return render(request, "base/home.html", context)
 
 
 def roomView(request, pk):
 
-    
-    context={}
-    return render(request,'base/room.html',context)
+    context = {}
+    return render(request, 'base/room.html', context)
 
 
 
-
+@login_required(login_url='login')
 def createRoomView(request):
 
     form = RoomForm()
@@ -31,15 +49,79 @@ def createRoomView(request):
     context = {'form': form}
     return render(request, 'base/create_room.html', context)
 
+# update the room
+# taking the primary key of the room that we want to update
 
 
 def updateRoomView(request, pk):
-    room=Room.objects.get(id=pk)
-    form=RoomForm(instance=room)
+    # get the room that matches with the primary key
+    room = Room.objects.get(id=pk)
+    # create a form that has the instance of the room
+    form = RoomForm(instance=room)
+    #redirecting the other user from using the updating option of a user
+    if request.user != room.host:
+        return redirect('home')
     if request.method == 'POST':
-        form=RoomForm(request.POST,instance=room)
+        # updating new data that has the same instance as room
+        form = RoomForm(request.POST, instance=room)
         if form.is_valid():
             form.save()
             return redirect('home')
-    context={'form':form}
-    return render(request,'base/create_room.html',context)
+    context = {'form': form}
+    return render(request, 'base/create_room.html', context)
+
+
+# Delete the room
+def deleteRoomView(request, pk):
+    room = Room.objects.get(id=pk)
+    #redirecting the other user from using the delete option of a user
+    if request.user != room.host:
+        return redirect('home')
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+
+    context = {'obj': room}
+    return render(request, 'base/delete.html', context)
+
+
+
+
+
+
+def loginpage(request):
+
+
+    if request.user.is_authenticated:
+        return redirect('home')
+
+
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        try:
+            user=User.objects.get(username=username)
+        except:
+            messages.error(request, 'User does not exist.')
+        
+        user=authenticate(request,username=username,password=password)
+
+        if user is not None:
+            login(request,user)  
+            return redirect('home')
+
+
+        else:
+            messages.error(request, 'Wrong Username or password.')
+
+    
+
+    context={}
+
+    return render(request,'base/login.html',context)
+
+
+def logoutpage(request):
+    logout(request)
+    return redirect('login')
